6465b71477e95fdc48cd2428ee86868f913dada0
Oluwaseun <annyauthe@yahoo.com>
2026-03-05 02:23:37 +0100

    Check user authentication

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -14,6 +14,9 @@ exports.protect = (req, res, next) => {
 };
 
 exports.adminOnly = (req, res, next) => {
+  if (!req.user) {
+    return res.status(401).json({ message: 'Not Authenticated' });
+  }
   if (req.user.role !== 'admin') {
     return res.status(403).json({ message: 'Admin only' });
   }
