be1f3641f3bbaf096eedeb044c07af9c879ee954
Oluwaseun <annyauthe@yahoo.com>
2026-03-05 02:16:31 +0100

    Create product dto

---
+++ src/dtos/product.dto.js
@@ -0,0 +1,16 @@
+exports.productToDTO = (product) => {
+  return {
+    id: product._id.toString(),
+    title: product.title,
+    description: product.description,
+    price: product.price,
+    stock: product.stock,
+    images: product.images,
+    category: product.category,
+    createdAt: product.createdAt
+  };
+};
+
+exports.productsToDTO = (products) => {
+  return products.map(exports.productToDTO);
+}
