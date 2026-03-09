3e208651f549b8a048e7f8a33c65d1b8007f32b8
Oluwaseun <annyauthe@yahoo.com>
2026-03-09 14:45:17 +0100

    Remove DTOs from controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,9 @@
 const AuthService = require('../services/auth.service');
-const { userToDTO } = require('../dtos/user.dto');
 
 exports.register = async (req, res) => {
   try {
     const result = await AuthService.register(req.body);
-    res.status(201).json(userToDTO(result));
+    res.status(201).json(result);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -13,7 +12,7 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   try {
     const result = await AuthService.login(req.body);
-    res.json(userToDTO(result));
+    res.json(result);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
