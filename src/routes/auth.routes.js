edab3879e5c7eec076ad6fbd960184f71ee85169
Oluwaseun <annyauthe@yahoo.com>
2026-03-25 23:13:04 +0100

    Update authentication

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 const controller = require('../controllers/auth.controller');
+const {
+  registerValidator,
+  loginValidator
+} = require('../validators/auth.validator');
+const {
+  handleValidationErrors
+} = require('../middlewares/validate.middleware');
 
-router.post('/register', controller.register);
-router.post('/login', controller.login);
+router.post('/register', registerValidator, handleValidationErrors, controller.register);
+router.post('/login', loginValidator, handleValidationErrors, controller.login);
 
 module.exports = router;
