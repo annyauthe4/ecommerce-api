d9362b57cf2b094b8ab10088e80787a2d8a7b82d
Oluwaseun <annyauthe@yahoo.com>
2026-03-25 23:10:49 +0100

    Modify product model

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,12 +1,36 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  price: Number,
-  stock: Number,
-  images: [String],
-  category: String
-}, { timestamps: true });
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+    stock: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+    category: {
+      type: String,
+      required: true
+    },
+    images: {
+      type: [String],
+      default: []
+    }
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Product', productSchema);
