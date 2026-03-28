5979a69895208845132d7d360de3217ae27b4d7c
Oluwaseun <annyauthe@yahoo.com>
2026-03-28 15:41:03 +0100

    Create order routes

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/order.controller');
-const { protect } = require('../middlewares/auth.middleware');
+const { protect, adminOnly } = require('../middlewares/auth.middleware');
 const { cancelOrderValidator } = require('../validators/order.validator');
 const { handleValidationErrors } = require('../middlewares/validate.middleware');
 
 router.post('/', protect, orderController.placeOrder);
 router.get('/my-orders', protect, orderController.getMyOrders);
+router.get('/admin/all', protect, adminOnly, orderController.getAllOrders);
+router.get('/:orderId', protect, orderController.getOrder);
 router.patch(
   '/:orderId/cancel',
   protect,
