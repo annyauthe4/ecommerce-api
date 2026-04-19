60a5fb905a302cb09e0582a78d9f13167423423e
Oluwaseun <annyauthe@yahoo.com>
2026-04-20 00:05:45 +0100

    Optimize the product controller

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,3 +1,4 @@
+const { search } = require('../routes/order.routes');
 const ProductService = require('../services/product.service');
 
 exports.createProduct = async (req, res, next) => {
@@ -14,8 +15,18 @@ exports.createProduct = async (req, res, next) => {
 
 exports.getProducts = async (req, res, next) => {
   try {
-    const products = await ProductService.getAllProducts();
-    res.json(products);
+    let { page, limit, category, search } = req.query;
+
+    if (search) {
+      search = search.replace(/^["']|["']$/g, '');
+    }
+    const products = await ProductService.getAllProducts(
+      parseInt(page) || 1,
+      parseInt(limit) || 10,
+      category,
+      search
+    );
+    res.status(200).json(products);
   } catch (err) {
     next(err);
   }
@@ -24,8 +35,15 @@ exports.getProducts = async (req, res, next) => {
 exports.getProduct = async (req, res, next) => {
   try {
     const product = await ProductService.getProductById(req.params.id);
-    res.json(product);
+    res.status(200).json(product);
   } catch (err) {
+    if (err.message === 'Product not found') {
+      return res.status(404).json({ message: err.message });
+    }
+    if (err.message.includes('format')) {
+      return res.status(400).json({ message: 'Invalid ID format' });
+    }
+
     next(err);
   }
 };
