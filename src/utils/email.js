c4e193001845a38601d6acd801573b31bf21a36a
Oluwaseun <annyauthe@yahoo.com>
2026-04-22 15:59:52 +0100

    Use Gmail to send mail

--- src/utils/email.js
+++ src/utils/email.js
@@ -4,8 +4,7 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
   // 1. Create a transporter
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
@@ -14,11 +13,10 @@ const sendEmail = async (options) => {
 
   // 2. Define email options
   const mailOptions = {
-    from: 'E-Commerce <noreply@mystore.com>',
+    from: `"KataKara E-Commerce Store" <${process.env.EMAIL_USER}>`,
     to: options.email,
     subject: options.subject,
-    text: options.message,
-    html: options.html, // Optional: for styled emails
+    html: options.html,
   };
 
   // 3. Send it
